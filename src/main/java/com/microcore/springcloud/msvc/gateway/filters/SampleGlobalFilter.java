1b32824850b06d3baafdb792d884ee6368a9ff26
juanmiloz <juanmiloz@hotmail.com>
2025-01-26 17:35:56 -0500

    feat: Add ordering to SampleGlobalFilter
    - Implemented the Ordered interface in the SampleGlobalFilter to define the execution order of the filter.
    - Set the filter's execution order to 100.
    - Ensures proper sequencing of filters when multiple filters are applied.

--- src/main/java/com/microcore/springcloud/msvc/gateway/filters/SampleGlobalFilter.java
+++ src/main/java/com/microcore/springcloud/msvc/gateway/filters/SampleGlobalFilter.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,7 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class SampleGlobalFilter implements GlobalFilter {
+public class SampleGlobalFilter implements GlobalFilter, Ordered {
 
     private final Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
 
@@ -25,5 +26,10 @@ public class SampleGlobalFilter implements GlobalFilter {
             exchange.getResponse().getCookies().add("color", ResponseCookie.from("color","red").build());
             exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
         }));
+    }
+
+    @Override
+    public int getOrder() {
+        return 100;
     }
 }
